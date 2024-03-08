c4fb1ac55721ca442ed0ba585bcddf1b9efa725b
enayetmorshedhaque <enayet4419@gmail.com>
2024-03-08 18:25:39 +0600

    working on homepage design. web large, small, and tablet view done.

---
+++ assets/js/theme/web-large.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+    $("#sliders-job-search-section").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+    $(".slider_second_section").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+    $(".SecondDivSliderNav").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+
+    $(".why_join_us_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+    });
+
+    $(".benefits_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: 5000,
+    });
+
+    $(".mission_vision_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+    });
+
+    $(".specialize_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+    });
+
+    $(".hiring_process_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: 5000,
+    });
+
+    $(".urgent_vacancy_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+    });
+
+    $(".testimonials_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+    });
+
+    $(".vacancy_news_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 1500,
+    });
+});
---
+++ assets/js/theme/web-small.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+    $("#web-small-sliders-job-search-section").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+    $(".web-small-slider_second_section").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+    $(".web-small-SecondDivSliderNav").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    $(".web-small-benefits_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: 5000,
+    });
+
+    $(".web_small_mission_vision_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+    });
+
+    $(".web-small-specialize_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+    });
+
+    $(".web-small-hiring_process_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: 5000,
+    });
+
+    $(".web-small-urgent_vacancy_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+    });
+
+    $(".web-small-testimonials_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+    });
+
+    $(".web-small-vacancy_news_slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 1500,
+    });
+});
